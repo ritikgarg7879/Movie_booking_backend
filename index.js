9b70f8145b55fa87b731307d2e1d386b38269448
Ritik Garg <gargritik855@gmail.com>
2026-01-27 23:51:32 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const env=require('dotenv')
+const mongoose=require('mongoose')
+
+
+env.config();
+const app=express();
+
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
+app.use(bodyParser.json())
+
+
+app.get("/home",(req,res)=>{
+  console.log("Hitting home");
+ return res.json({
+    success:true,
+    message:"Fetched home"
+  });
+});
+
+
+
+//In this remember it is the way in hich we can connect mogngodb with help of async await
+// app.listen(process.env.PORT,async()=>{
+//   console.log("Server is running on 3000");
+
+//  await mongoose.connect(process.env.DB_URL)
+//  console.log("Successfully connected to mongo");
+// });
+
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server is running on ${process.env.PORT}`);
+
+//   mongoose.connect(process.env.DB_URL)
+//     .then(() => {
+//       console.log("Successfully connected to MongoDB");
+//     })
+//     .catch((err) => {
+//       console.log("Error while connecting to MongoDB", err);
+//     });
+// });
+
+app.listen(process.env.PORT, async () => {
+  console.log(`Server is running on ${process.env.PORT}`);
+
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("Successfully connected to MongoDB");
+  } catch (err) {
+    console.log("Error while connecting to MongoDB", err);
+  }
+});
+
+
