c4366f18f134327c19cc2c421874f91483c4d007
Ritik Garg <gargritik855@gmail.com>
2026-01-29 23:38:12 +0530

    Feat:handeled validation properties and middleware

---
+++ middlewares/theatre.middleware.js
@@ -0,0 +1,46 @@
+const badRequestResponse={
+  success:false,
+  err:{},
+  data:{},
+  message:"Malformed Request | Bad Request"
+}
+
+const validateTheatreCreateRequest= async(req,res,next)=>{
+  
+  //validate the theatre name
+  if(!req.body.name){
+    badRequestResponse.err="The name of the theatre is not present in the request"
+    return res.status(400).json(badRequestResponse);
+  }
+
+  //validate the theatre description
+  if(!req.body.description){
+    badRequestResponse.err="The description of the theatre is not present in the request"
+    return res.status(400).json(badRequestResponse);
+  }
+
+  //validate the theatre city
+  if(!req.body.city){
+    badRequestResponse.err="The city of the theatre is not present in the request"
+    return res.status(400).json(badRequestResponse);
+  }
+
+  //validate the theatre pincode
+  if(!req.body.pincode){
+    badRequestResponse.err="The pincode of the movie is not present in the request"
+    return res.status(400).json(badRequestResponse);
+  }
+
+  //validate the address of the theatre
+  if(!req.body.address){
+    badRequestResponse.err="The address of the movie is not present in the request"
+    return res.status(400).json(badRequestResponse);
+  }
+
+  next();
+  
+}
+
+module.exports={
+  validateTheatreCreateRequest
+}
--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -1,8 +1,8 @@
 const theatreController=require('../controllers/theatre.controller');
-
+const theatreMiddlewares=require('../middlewares/theatre.middleware');
 
 const routes=(app)=>{
-  app.post('/mba/api/v1/theatres',theatreController.createTheatre);
+  app.post('/mba/api/v1/theatres',theatreMiddlewares.validateTheatreCreateRequest,theatreController.createTheatre);
 }
 
 module.exports=routes
