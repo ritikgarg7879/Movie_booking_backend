5b2a5fc971a2d12671099dd25849d3c77b98320a
Ritik Garg <gargritik855@gmail.com>
2026-01-30 23:45:18 +0530

    Added api to update movie in theatre

--- services/theatre.service.js
+++ services/theatre.service.js
@@ -176,31 +176,65 @@ const updateTheatre=async(id,data)=>{
 
 const updateMoviesInTheatres=async(theatreId,movieIds,insert)=>{
 
-  const theatre=await Theatre.findById(theatreId);
-  if(!theatre){
-    return {
-      err:"No such theatre found for the id provided",
-      code:404
-    };
-  }
+  // const theatre=await Theatre.findById(theatreId);
+  // if(!theatre){
+  //   return {
+  //     err:"No such theatre found for the id provided",
+  //     code:404
+  //   };
+  // }
  
-  if (insert) {
-    // ADD movies
-    movieIds.forEach(movieId => {
-      if (!theatre.movies.includes(movieId)) {
-        theatre.movies.push(movieId);
-      }
-    });
-  } else {
-    // REMOVE movies ✅✅
-    theatre.movies = theatre.movies.filter(
-      existingMovieId =>
-        !movieIds.includes(existingMovieId.toString())
-    );
-  }
+  // if (insert) {
+  //   // ADD movies
+  //   movieIds.forEach(movieId => {
+  //     if (!theatre.movies.includes(movieId)) {
+  //       theatre.movies.push(movieId);
+  //     }
+  //   });
+  // } else {
+  //   // REMOVE movies 
+  //   theatre.movies = theatre.movies.filter(
+  //     existingMovieId =>
+  //       !movieIds.includes(existingMovieId.toString())
+  //   );
 
-  await theatre.save();
-  return theatre.populate('movies');
+  // }
+
+  //   const theatre=await Theatre.findById(theatreId);
+  //   return theatre.populate('movies');
+
+  // await theatre.save();
+  // return theatre.populate('movies');
+
+
+  // The problem with above functionality is they are adding same movie more than one time
+
+    try{
+    if(insert){
+      //we need to add movie
+      await Theatre.updateOne(
+        {_id: theatreId},
+        {$addToSet:{movies:{$each: movieIds}}}
+      );
+    }else{
+      //we need to remove movie
+      await Theatre.updateOne(
+        {_id: theatreId},
+        {$pull:{movies:{$in:movieIds}}}
+      );
+    }
+    const theatre=await Theatre.findById(theatreId);
+    return theatre.populate('movies');
+  }catch(error){
+    if(error.name=="TypeError"){
+      return {
+        code:404,
+        err:"No theatre found for the given id"
+      }
+    }
+    console.log("Error is",error);
+    throw error;
+  }
 }
 
 
