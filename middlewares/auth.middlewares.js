93c54b569515993a35897171bb2a3ff605758a68
Ritik Garg <gargritik855@gmail.com>
2026-01-31 20:33:29 +0530

    Feat: Added isAuthenticated middleware for checking api auth

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -5,6 +5,9 @@ const badRequestResponse={
   message:"Malformed Request | Bad Request"
 }
 
+const jwt = require('jsonwebtoken');
+const userService=require('../services/user.service');
+
 /**
  * 
  * @param  req ->http request object
@@ -37,6 +40,15 @@ const validateSignupRequest= async(req,res,next)=>{
   
 }
 
+/**
+ * 
+ * @param  req ->http request object
+ * @param  res ->http response object
+ * @param  next ->next middleware 
+ * @returns 
+ */
+
+
 const validateSigninRequest= async(req,res,next)=>{
   
   //validate the User email
@@ -56,6 +68,36 @@ const validateSigninRequest= async(req,res,next)=>{
 }
 
 
+const isAuthenticated= async(req,res,next) =>{
+  try {
+  const token=req.headers["x-access-token"];
+  if(!token){
+    badRequestResponse.err="No token provided";
+    return res.status(403).json(badRequestResponse);
+  }
 
-module.exports={validateSignupRequest,validateSigninRequest};
+  const response=jwt.verify(token,process.env.AUTH_KEY);
+  if(!response){
+    badRequestResponse.err="Token not verified";
+    return res.status(401).json(badRequestResponse);
+  }
+  const user=await userService.getUserById(response.id);
+  req.user=user.id;
+  next();
+  } catch (error) {
+    if(error.name=="JsonWebTokenError"){
+      badRequestResponse.err=error.message;
+      return res.status(401).json(badRequestResponse);
+    }
+    if(error.code==404){
+      badRequestResponse.err="User doesn't existI ";
+      return res.status(error.code).json(badRequestResponse);
+    }
+    badRequestResponse.err=error;
+    return res.status(500).json(badRequestResponse)
+  }
+  
+}
+
+module.exports={validateSignupRequest,validateSigninRequest,isAuthenticated};
 
