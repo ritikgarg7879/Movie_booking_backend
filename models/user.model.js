7ddfeaf3aeeadd4ee50868cb652ab67a1edc2674
Ritik Garg <gargritik855@gmail.com>
2026-01-31 16:06:17 +0530

    Encrypted plain password using register

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
 const {Schema}=mongoose;
+const bcrypt=require('bcrypt')
 
 
 const userSchema=new Schema({
@@ -36,6 +37,29 @@ const userSchema=new Schema({
     }
 
 },{timestamps:true});
+
+
+
+//In this before new user is made we are encrypting the password
+userSchema.pre('save',async function (next){
+  //a trigger to encrypt plain password before saving the user
+
+  if (!this.isModified('password')) {
+    return ;
+  }
+
+
+  // // console.log(this)
+  // const hash=await bcrypt.hash(this.password,10);
+  // // console.log(hash);
+  // this.password=hash;
+  // // console.log(this);
+  // next();
+
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+});
 
 const User=mongoose.model('User',userSchema);
 module.exports=User;
