ce45fb59352c475ca958e212eda6fe2b4159773f
Ritik Garg <gargritik855@gmail.com>
2026-02-01 14:26:35 +0530

    Feat: Added validation for movies

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,11 +1,13 @@
 const MovieController=require('../controllers/movie.controller');
-const MovieMiddlewares=require('../middlewares/movie.middlewares')
+const MovieMiddlewares=require('../middlewares/movie.middlewares');
+const authMiddleWares=require('../middlewares/auth.middlewares');
+
 
 const routes=(app)=>{
   //routes function takes express app object as parameter
 
   //CREATE
-  app.post('/mba/api/v1/movies',MovieMiddlewares.validateMovieCreateRequest,MovieController.createMovie);
+  app.post('/mba/api/v1/movies',authMiddleWares.isAuthenticated,authMiddleWares.isAdminOrClient,MovieMiddlewares.validateMovieCreateRequest,MovieController.createMovie);
   
   //DELETE
   app.delete('/mba/api/v1/movies/:movieId',MovieController.deleteMovie);
